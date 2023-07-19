dd728f5dd523e1d44faa39603d872a91918d921b
Neshnjenga <chegenelson641@gmail.com>
2023-07-18 23:26:33 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - prints alphabet in lower case
+*Return: no return
+*/
+void print_alphabet(void)
+{
+int alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar (alphabet);
+}
+_putchar (10);
+}
